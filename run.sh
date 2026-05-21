430ec92cf1d7877fc7031f6e93984be8041c78a4
slynn1324 <slynn1324@gmail.com>
2026-05-20 21:14:02 -0500

    add notes

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# I use podman instead of docker. with podman rootless, UID/GID=0 will end up being
+# the host user id.  I don't recommend running as root on a docker setup.
+# if you setup ssh keys instead of mapping the socket then you don't need uid=0 eiher.
 podman run -d \
   --name virt-manager \
   -e PUID=0 \
